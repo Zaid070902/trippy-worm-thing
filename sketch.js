0691273e9f0ac93e3db12723d8d25b8c66701858
Zaid <zaid.flandorp@rogerwilco.co.za>
2024-08-27 19:58:49 +0200

    mouse interaction

--- sketch.js
+++ sketch.js
@@ -5,41 +5,20 @@ let n, red, blue, green, rN, gN, bN, Sz;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(11);
-  red = 0.004;
+  red = 0.009;
   green = 0.006;
-  blue = 0.005;
-  nsz = 0.02;
+  blue = 0.007;
+  nsz = 0.01;
+}
 
+function mouseDragged() {
+  balls.push(new Ball(mouseX, mouseY));
 
-  for (let i = 0; i < 500; i++) {
-    balls[i] = {
-      x: random(0, width),
-      y: random(0, height),
-      size: random(0, 200),
-      display: function () {
-        rN = noise(this.x * red, this.y * red) * 260;
-        gN = noise(this.x * green, this.y * green) * 260;
-        bN = noise(this.x * blue, this.y * blue) * 260;
-        Sz = noise(this.x * nsz, this.y * nsz) * 50;
+}
 
-
-        fill(rN, gN, bN, 50);
-        strokeWeight(1);
-        stroke(0, 0, 0, 10);
-        ellipse(this.x, this.y, Sz);
-      },
-      move: function () {
-        let dirX = 0.07;
-        let dirY = 0.05;
-
-        let nX = noise(this.x * dirX, this.y * dirY) * 4 - 2;
-        let nY = noise(this.y * dirX, this.y * dirY) * 4 - 2;
-
-        constrain(this.x += nX, 0, width - 50);
-        constrain(this.y += nY, 0, height - 50);
-      }
-    }
-  }
+function mousePressed() {
+  balls.length = 0;
+  background(11);
 }
 
 function draw() {
@@ -49,4 +28,79 @@ function draw() {
     balls[i].display();
     balls[i].move();
   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Constructor Functions
+
+
+function Ball(x, y) {
+
+  this.x = x,
+    this.y = y,
+    this.display = function () {
+      rN = noise(this.x * red, this.y * red) * 260;
+      gN = noise(this.x * green, this.y * green) * 260;
+      bN = noise(this.x * blue, this.y * blue) * 260;
+      Sz = noise(this.x * nsz, this.y * nsz) * 70;
+
+
+      fill(rN, gN, bN, 90);
+      strokeWeight(1);
+      stroke(0, 0, 0, 10);
+      ellipse(this.x, this.y, Sz);
+    },
+    this.move = function () {
+      let dirX = 0.05;
+      let dirY = 0.03;
+
+      let nX = noise(this.x * dirX, this.y * dirY) * 6 - 3;
+      let nY = noise(this.y * dirX, this.y * dirY) * 6 - 3;
+
+      constrain(this.x += nX, 0, width - 50);
+      constrain(this.y += nY, 0, height - 50);
+    }
 }
