b6bbea47ae734797fe585f3afe64a967b47f67b8
Zaid <zaid.flandorp@rogerwilco.co.za>
2024-08-28 18:55:25 +0200

    added ball object into a separate file

--- sketch.js
+++ sketch.js
@@ -1,6 +1,4 @@
 let balls = [];
-let n, red, blue, green, rN, gN, bN, Sz;
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -18,83 +16,4 @@ function draw() {
     balls[i].display();
     balls[i].move();
   }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Constructor Functions
-
-
-function Ball(x, y) {
-
-  this.x = x,
-    this.y = y,
-    this.display = function () {
-      red = 0.007;
-      green = 0.003;
-      blue = 0.005;
-      nsz = 0.05;
-
-      rN = noise(this.x * red, this.y * red) * 260;
-      gN = noise(this.x * green, this.y * green) * 260;
-      bN = noise(this.x * blue, this.y * blue) * 260;
-      Sz = noise(this.x * nsz, this.y * nsz) * 30;
-
-      fill(rN, gN, bN, 90);
-      strokeWeight(1);
-      stroke(0, 0, 0, 50);
-      ellipse(this.x, this.y, Sz);
-    },
-    this.move = function () {
-      let dirX = 0.07;
-      let dirY = 0.09;
-
-      let nX = noise(this.x * dirX, this.y * dirY) * 2 - 1;
-      let nY = noise(this.y * dirX, this.y * dirY) * 3 - 1.5;
-
-      constrain(this.x += nX, 0, width - 50);
-      constrain(this.y += nY, 0, height - 50);
-    }
 }
