1c697ec935ea6ad402ca08c5cb0b935cea6d8bf2
Zaid <zaid.flandorp@rogerwilco.co.za>
2024-10-28 11:00:11 +0200

    butterfly :)

--- Balls.js
+++ Balls.js
@@ -1,33 +1,33 @@
-let n, red, blue, green, rN, gN, bN, Sz;
+let red, blue, green, rN, gN, bN, Sz;
 
 function Ball(x, y) {
+  this.x = x;
+  this.y = y;
 
-  this.x = x,
-    this.y = y,
-    this.display = function () {
-      red = 0.001;
-      green = 0.005;
-      blue = 0.002;
-      nsz = 0.03;
+  this.display = function () {
+    red = 0.001;
+    green = 0.005;
+    blue = 0.002;
+    nsz = 0.03;
 
-      rN = noise(this.x * red, this.y * red) * 500;
-      gN = noise(this.x * green, this.y * green) * 300;
-      bN = noise(this.x * blue, this.y * blue) * 500;
-      Sz = noise(this.x * nsz, this.y * nsz) * 15;
+    rN = noise(this.x * red, this.y * red) * 500;
+    gN = noise(this.x * green, this.y * green) * 300;
+    bN = noise(this.x * blue, this.y * blue) * 480;
+    Sz = noise(this.x * nsz, this.y * nsz) * 5;
 
-      strokeWeight(Sz);
-      stroke(rN, gN, bN, Sz);
-      point(this.x, this.y);
-    },
+    strokeWeight(Sz);
+    stroke(rN + 100, gN - 100, bN, 5);
+    point(this.x, this.y);
+  };
 
-    this.move = function () {
-      let dirX = 0.05;
-      let dirY = 0.04;
+  this.move = function () {
+    let dirX = 0.05;
+    let dirY = 0.03;
 
-      let nX = noise(this.x * dirX, this.y * dirY) * 10 - 5;
-      let nY = noise(this.y * dirX, this.y * dirY) * 5 - 3.5;
+    let nX = noise(this.x * dirX, this.y * dirY) * 4 - 2;
+    let nY = noise(this.y * dirX, this.y * dirY) * 2 - 1;
 
-      this.x = constrain(this.x += nX, 0, width);
-      this.y = constrain(this.y += nY, 0, height);
-    }
+    this.x = constrain(this.x + nX, 0, width);
+    this.y = constrain(this.y + nY, 0, height);
+  };
 }
--- sketch.js
+++ sketch.js
@@ -1,23 +1,29 @@
 let balls = [];
-let VSpacing = 3;
-let HSpacing = 3;
+let numPoints = 3500;
+let scale = 30;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(11);
+  background(50);
+  frameRate(30);
 }
 
 function mousePressed() {
-  for (let i = 0; i < 50; i++) {
-    balls.push(new Ball(mouseX, mouseY + i * VSpacing));
-    for (let j = 0; j < 5; j++) {
-      balls.push(new Ball(mouseX + j * HSpacing, mouseY + i * VSpacing));
-    }
-  };
+  for (let i = 0; i < numPoints; i++) {
+    let t = map(i, 0, numPoints, 0, TWO_PI * 12);
+    let x =
+      scale * (sin(t) * (exp(cos(t)) - 3 * cos(4 * t) - pow(sin(t / 12), 5))) +
+      mouseX;
+    let y =
+      scale * (cos(t) * (exp(cos(t)) - 2 * cos(4 * t) - pow(sin(t / 12), 5))) +
+      mouseY;
 
-};
+    balls.push(new Ball(x, y));
+  }
+}
 
 function draw() {
+  // blendMode(DIFFERENCE);
   for (let i = 0; i < balls.length; i++) {
     balls[i].display();
     balls[i].move();
